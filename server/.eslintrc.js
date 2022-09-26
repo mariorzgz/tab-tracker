2c945d7f1a1aa1b7c174ddfab81a756da809cc72
mariorzgz <mario.rod.gonz97@gmail.com>
2022-09-26 20:38:18 +0200

    Eslint config

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -1,12 +1,9 @@
-export default {
+module.exports = {
     "env": {
         "browser": true,
         "es2021": true
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:vue/vue3-essential"
-    ],
+    "extends": "plugin:vue/vue3-essential",
     "overrides": [
     ],
     "parserOptions": {
